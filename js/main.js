1a44f1b29f76c863e3ba6322a8018be8684bfd57
Joseph Valdez <Joseph.W.Valdez@Gmail.com>
2023-01-11 00:37:53 +0000

    removed nonfunctional features

--- js/main.js
+++ js/main.js
@@ -1,26 +1,3 @@
-var $checkboxButtons = document.querySelectorAll('.checkbox');
-function handleCheckedBox(event) {
-  for (let index = 0; index < $checkboxButtons.length; index++) {
-    $checkboxButtons[index].classList.remove('checked-box');
-  }
-  var $checkboxTarget = event.target.parentNode.parentNode.getAttribute('id');
-  $checkboxTarget = '.checkbox-' + $checkboxTarget;
-  $checkboxTarget = document.querySelector($checkboxTarget);
-  $checkboxTarget.classList.add('checked-box');
-}
-
-var $noFilter = document.querySelector('#no-filter');
-var $controllers = document.querySelector('#controllers');
-var $duelists = document.querySelector('#duelists');
-var $initiators = document.querySelector('#initiators');
-var $sentinels = document.querySelector('#sentinels');
-
-$noFilter.addEventListener('click', handleCheckedBox);
-$controllers.addEventListener('click', handleCheckedBox);
-$duelists.addEventListener('click', handleCheckedBox);
-$initiators.addEventListener('click', handleCheckedBox);
-$sentinels.addEventListener('click', handleCheckedBox);
-
 var $viewNodes = document.querySelectorAll('.view');
 var $navBar = document.querySelector('.navbar');
 
