47287b2f06a8ce4168ed82c5c0c2840c57c819af
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-05-10 20:18:01 +0000

    set up react components and pages structures and set up tailwind

---
+++ react-app/src/pages/homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const homepage = () => {
+  return (
+    <>
+      <div className='navbar flex'>
+        <h3 className='bold'>Valochecker</h3>
+        <h5>Agents</h5>
+        <h5>Weapons</h5>
+      </div>
+      <h1>Homepage</h1>
+    </>
+  )
+}
+
+export default homepage
