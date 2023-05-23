cafc61965becf9e503205b151c658724e0aced2a
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-05-23 21:56:02 +0000

    adjusted positioning for full agent portrait elements

--- react-app/src/components/FullAgentPortrait.jsx
+++ react-app/src/components/FullAgentPortrait.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const FullAgentPortrait = ({ agent }) => {
 
   return (
-    <div className='relative w-[700px] h-[700px] overflow-hidden'>
-      <div className='w-[500px] absolute overflow-hidden left-[120px]'>
+    <div className='relative w-[700px] h-[600px] overflow-hidden transform scale-100 sm:scale-125'>
+      <div className='w-[400px] absolute overflow-hidden left-[-50px] top-[-34px]'>
         <img src={agent.background} alt={`${agent.displayName} background`} className='object-contain'/>
       </div>
-      <div className='w-[600px] absolute top-[80px] left-[75px] overflow-hidden'>
+      <div className='w-[600px] absolute top-[8px] left-[-134px] overflow-hidden'>
         <img src={agent.fullPortraitV2} alt={`${agent.displayName} portrait`} className='object-contain w-full' />
       </div>
     </div>
