a88e76c8e6e774ec6cdfae7c555d270ded5585d7
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-06-06 12:37:44 +0000

    adjusted individual weapon code to use the param to fetch the data instead of using location

--- react-app/src/pages/IndividualWeapon.jsx
+++ react-app/src/pages/IndividualWeapon.jsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import BackButton from '../components/BackButton';
 import Header from '../components/Header';
 import FlexBasisFull from '../components/FlexBasisFull';
 import DataTable from '../components/DataTable';
 import { convertCamelCase, convertContainsColons, roundLongDecimals } from '../utilities/stringConversions';
 import { meleeStats } from '../data/meleeInfo';
+import { fetchWeapon } from '../utilities/FetchWeapons';
 
 const IndividualWeapon = () => {
-  const location = useLocation();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const { weaponName } = useParams();
+  const [weaponData, setWeaponData] = useState(null);
 
-  const weapon = location.state.data;
-  const weaponStats = weapon.weaponStats;
-  const weaponIconWidth = weapon.shopData?.categoryText === 'Sidearms' ? 'w-[200px]' : 'w-[500px]';
+  useEffect(() => {
+    const getWeaponData = async () => {
+      try {
+        const weaponData = await fetchWeapon(weaponName);
+        setWeaponData(weaponData);
+      } catch (error) {
+        console.error(error);
+        setWeaponData(null);
+      }
+    };
+
+    getWeaponData();
+  }, [weaponName]);
+
+  if (!weaponData) {
+    return null; // Render a loading state or an error message
+  }
+
+  const weaponStats = weaponData.weaponStats;
+  const weaponIconWidth = weaponData.shopData?.categoryText === 'Sidearms' ? 'w-[200px]' : 'w-[500px]';
 
   const convertWeaponStats = (weaponStats) => {
     /* If the weapon stats are null, return the melee stats */
     if (!weaponStats) {
-      return meleeStats
+      return meleeStats;
     }
     const convertedStats = {};
 
@@ -26,12 +45,13 @@ const IndividualWeapon = () => {
       const convertedProp = convertCamelCase(prop);
       let convertedValue = convertContainsColons(value);
       convertedValue = convertCamelCase(convertedValue);
-      convertedValue = roundLongDecimals(convertedValue)
+      convertedValue = roundLongDecimals(convertedValue);
       // Exclude key-value pair if the value is null, NaN, or matches other exclusion rules
       if (
-        convertedValue !== null && !isNaN(convertedValue) &&
+        convertedValue !== null &&
+        !isNaN(convertedValue) &&
         !(
-          (convertedProp === "Burst Count" || convertedProp === "Shotgun Pellet Count") &&
+          (convertedProp === 'Burst Count' || convertedProp === 'Shotgun Pellet Count') &&
           convertedValue === 1
         )
       ) {
@@ -72,19 +92,19 @@ const IndividualWeapon = () => {
   const weaponStatsArray = Object.entries(convertedStats);
 
   const handleWeaponSkinsButton = () => {
-    navigate('/weapon-skins', { state: { data: weapon} })
-  }
+    navigate('/weapon-skins', { state: { data: weaponData } });
+  };
 
   return (
     <>
       <div className=''>
         <div className='flex items-center justify-center'>
           <BackButton />
-          <Header text={weapon.displayName} />
+          <Header text={weaponData.displayName} />
         </div>
         <FlexBasisFull />
         <div className={`${weaponIconWidth} max-w-[95vw]`}>
-          <img src={weapon.displayIcon} alt={weapon.displayName} className='object-contain w-full' />
+          <img src={weaponData.displayIcon} alt={weaponData.displayName} className='object-contain w-full' />
         </div>
         <FlexBasisFull />
         <button
--- react-app/src/utilities/FetchWeapons.js
+++ react-app/src/utilities/FetchWeapons.js
@@ -14,3 +14,15 @@ export const fetchWeapons = async (selectedOption, setWeapons) => {
     console.error(error);
   }
 };
+
+export const fetchWeapon = async (weaponName) => {
+  try {
+    const response = await fetch(`https://valorant-api.com/v1/weapons?displayName=${weaponName}`);
+    const data = await response.json();
+    const weapon = data.data.find(weapon => weapon.displayName.toLowerCase() === weaponName.toLowerCase());
+    return weapon || null;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
