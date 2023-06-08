971492fcae6fd4e41da9a3f31fdd8c9b72f4608b
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-06-08 03:32:19 +0000

    removed obsolete folders and files

--- react-app/src/components/Subheader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Subheader = ({text}) => {
-  return (
-    <h1 className='select-none mb-4 text-lg sm:5xl'>{text}</h1>
-  )
-}
-
-export default Subheader
--- react-app/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  ignoreFiles: ['./public/output.css'],
-  content: ["./src/**/*.{html,js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
