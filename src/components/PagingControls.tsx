f56aae34c76736dabe6071ec4785b81c9794e0fa
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-06-19 21:48:52 +0000

    styled the paging controls

--- src/components/PagingControls.tsx
+++ src/components/PagingControls.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
 type PageControlProps = {
   currentPage: number;
@@ -8,71 +9,78 @@ type PageControlProps = {
 
 const PageControls: React.FC<PageControlProps> = ({ currentPage, totalPages, onPageChange }) => {
   const handlePageChange = (pageNumber: number) => {
-    // Check if the page number is within the valid range
     if (pageNumber >= 1 && pageNumber <= totalPages) {
-      // Call the onPageChange callback function provided by the parent component
       onPageChange(pageNumber);
     }
   };
 
   return (
-    <div className="w-full flex justify-center px-4 py-2">
-      {/* Previous button */}
-      <button
-        onClick={() => handlePageChange(currentPage - 1)}
-        disabled={currentPage === 1}
-        className="mx-1"
-      >
-        Previous
-      </button>
-
-      {/* Rendering the page 1 button */}
-      {currentPage > 2 && (
-        <>
-          <button onClick={() => handlePageChange(1)} className="mx-1">
-            1
-          </button>
-          {currentPage > 3 && <span className="mx-1">...</span>}
-        </>
-      )}
-
-      {/* Rendering the previous page button */}
-      {currentPage > 1 && (
-        <button onClick={() => handlePageChange(currentPage - 1)} className="mx-1">
-          {currentPage - 1}
+    <div className="w-full flex justify-center mx-2 py-2">
+      <div className="border border-2 rounded flex items-center">
+        {/* Previous button */}
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+          className="px-4 py-2 flex items-center hover:bg-gray-700 transition duration-100"
+        >
+          <FiChevronLeft />
+          Previous
         </button>
-      )}
+         {/* Line separator */}
+        <div className="border-r h-4 mx-1"></div>
 
-      {/* Rendering the current page button */}
-      <button onClick={() => handlePageChange(currentPage)} className="mx-1 font-bold" disabled>
-        {currentPage}
-      </button>
+        {/* Rendering the page 1 button */}
+        {currentPage > 2 && (
+          <>
+            <button onClick={() => handlePageChange(1)} className="px-2 py-2  hover:bg-gray-700 transition duration-100">
+              1
+            </button>
+            {currentPage > 3 && <span className="px-2 py-2 ">...</span>}
+          </>
+        )}
 
-      {/* Rendering the next page button */}
-      {currentPage < totalPages && (
-        <button onClick={() => handlePageChange(currentPage + 1)} className="mx-1">
-          {currentPage + 1}
-        </button>
-      )}
-
-      {/* Rendering the last page button */}
-      {currentPage < totalPages - 1 && (
-        <>
-          {currentPage < totalPages - 2 && <span className="mx-1">...</span>}
-          <button onClick={() => handlePageChange(totalPages)} className="mx-1">
-            {totalPages}
+        {/* Rendering the previous page button */}
+        {currentPage > 1 && (
+          <button onClick={() => handlePageChange(currentPage - 1)} className="px-2 py-2  hover:bg-gray-700 transition duration-100">
+            {currentPage - 1}
           </button>
-        </>
-      )}
+        )}
 
-      {/* Next button */}
-      <button
-        onClick={() => handlePageChange(currentPage + 1)}
-        disabled={currentPage === totalPages}
-        className="mx-1"
-      >
-        Next
-      </button>
+        {/* Rendering the current page button */}
+        <button onClick={() => handlePageChange(currentPage)} className="px-2 py-2  font-bold" disabled>
+          {currentPage}
+        </button>
+
+        {/* Rendering the next page button */}
+        {currentPage < totalPages && (
+          <button onClick={() => handlePageChange(currentPage + 1)} className="px-2 py-2  hover:bg-gray-700 transition duration-100">
+            {currentPage + 1}
+          </button>
+        )}
+
+        {/* Rendering the last page button */}
+        {currentPage < totalPages - 1 && (
+          <>
+            {currentPage < totalPages - 2 && <span className="px-2 py-2 ">...</span>}
+            <button onClick={() => handlePageChange(totalPages)} className="px-2 py-2  hover:bg-gray-700 transition duration-100">
+              {totalPages}
+            </button>
+          </>
+        )}
+
+         {/* Line separator */}
+        <div className="border-r h-4 mx-1"></div>
+
+        {/* Next button */}
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+          className="px-4 py-2  flex items-center hover:bg-gray-700 transition duration-100"
+        >
+          Next
+          <FiChevronRight />
+        </button>
+      </div>
     </div>
   );
 };
