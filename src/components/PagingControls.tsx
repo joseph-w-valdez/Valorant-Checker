80c876bd0b5560d2bf251306492c3862124043b9
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-06-19 21:50:55 +0000

    fixed bug with hover effects if user is on first or last pages respectively

--- src/components/PagingControls.tsx
+++ src/components/PagingControls.tsx
@@ -21,39 +21,50 @@ const PageControls: React.FC<PageControlProps> = ({ currentPage, totalPages, onP
         <button
           onClick={() => handlePageChange(currentPage - 1)}
           disabled={currentPage === 1}
-          className="px-4 py-2 flex items-center hover:bg-gray-700 transition duration-100"
+          className={`px-4 py-2 flex items-center ${
+            currentPage === 1 ? '' : 'hover:bg-gray-700 transition duration-100'
+          }`}
         >
           <FiChevronLeft />
           Previous
         </button>
-         {/* Line separator */}
+        {/* Line separator */}
         <div className="border-r h-4 mx-1"></div>
 
         {/* Rendering the page 1 button */}
         {currentPage > 2 && (
           <>
-            <button onClick={() => handlePageChange(1)} className="px-2 py-2  hover:bg-gray-700 transition duration-100">
+            <button
+              onClick={() => handlePageChange(1)}
+              className={`px-2 py-2 ${currentPage === 1 ? 'font-bold' : 'hover:bg-gray-700 transition duration-100'}`}
+            >
               1
             </button>
-            {currentPage > 3 && <span className="px-2 py-2 ">...</span>}
+            {currentPage > 3 && <span className="px-2 py-2">...</span>}
           </>
         )}
 
         {/* Rendering the previous page button */}
         {currentPage > 1 && (
-          <button onClick={() => handlePageChange(currentPage - 1)} className="px-2 py-2  hover:bg-gray-700 transition duration-100">
+          <button
+            onClick={() => handlePageChange(currentPage - 1)}
+            className={`px-2 py-2 ${currentPage === 1 ? 'font-bold' : 'hover:bg-gray-700 transition duration-100'}`}
+          >
             {currentPage - 1}
           </button>
         )}
 
         {/* Rendering the current page button */}
-        <button onClick={() => handlePageChange(currentPage)} className="px-2 py-2  font-bold" disabled>
+        <button onClick={() => handlePageChange(currentPage)} className="px-2 py-2 font-bold" disabled>
           {currentPage}
         </button>
 
         {/* Rendering the next page button */}
         {currentPage < totalPages && (
-          <button onClick={() => handlePageChange(currentPage + 1)} className="px-2 py-2  hover:bg-gray-700 transition duration-100">
+          <button
+            onClick={() => handlePageChange(currentPage + 1)}
+            className={`px-2 py-2 ${currentPage === totalPages ? 'font-bold' : 'hover:bg-gray-700 transition duration-100'}`}
+          >
             {currentPage + 1}
           </button>
         )}
@@ -61,21 +72,26 @@ const PageControls: React.FC<PageControlProps> = ({ currentPage, totalPages, onP
         {/* Rendering the last page button */}
         {currentPage < totalPages - 1 && (
           <>
-            {currentPage < totalPages - 2 && <span className="px-2 py-2 ">...</span>}
-            <button onClick={() => handlePageChange(totalPages)} className="px-2 py-2  hover:bg-gray-700 transition duration-100">
+            {currentPage < totalPages - 2 && <span className="px-2 py-2">...</span>}
+            <button
+              onClick={() => handlePageChange(totalPages)}
+              className={`px-2 py-2 ${currentPage === totalPages ? 'font-bold' : 'hover:bg-gray-700 transition duration-100'}`}
+            >
               {totalPages}
             </button>
           </>
         )}
 
-         {/* Line separator */}
+        {/* Line separator */}
         <div className="border-r h-4 mx-1"></div>
 
         {/* Next button */}
         <button
           onClick={() => handlePageChange(currentPage + 1)}
           disabled={currentPage === totalPages}
-          className="px-4 py-2  flex items-center hover:bg-gray-700 transition duration-100"
+          className={`px-4 py-2 flex items-center ${
+            currentPage === totalPages ? '' : 'hover:bg-gray-700 transition duration-100'
+          }`}
         >
           Next
           <FiChevronRight />
