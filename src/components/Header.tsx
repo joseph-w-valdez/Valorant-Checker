72a6b91e8bd743171d220638e980b13fda032e91
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-06-20 01:01:59 +0000

    adjusted responsiveness on header for small screen sizes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ text }) => {
   return (
-    <h1 className='select-none text-3xl sm:text-5xl my-6 mx-0 sm:mx-6'>{text}</h1>
+    <h1 className='select-none text-3xl sm:text-5xl my-6 mx-0 sm:mx-6 max-w-[60%] md:max-w-none'>{text}</h1>
   );
 };
 
