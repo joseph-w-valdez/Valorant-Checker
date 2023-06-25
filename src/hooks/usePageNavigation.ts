b2111592fcc445d182251e4ba30a7688db8c57c0
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-06-25 18:41:36 +0000

    custom hook for page navigation implemented properly

--- src/hooks/usePageNavigation.ts
+++ src/hooks/usePageNavigation.ts
@@ -1,37 +1,43 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 export const usePageNavigation = (
-  totalPages: number,
-  parsedPage: number,
-  currentPage: number,
-  searchParam: string,
-  navigate: (to: string) => void,
-  setCurrentPage: (page: number) => void,
+  filteredDataLength: number,
   pageSize: number,
-  filterValue: string
+  searchValue: string,
 ) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const pageParam = queryParams.get('page');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [desiredPage, setDesiredPage] = useState(1);
+
   useEffect(() => {
+    const totalPages = Math.ceil(filteredDataLength / pageSize);
+    const parsedPage = pageParam ? parseInt(pageParam, 10) : 1;
+    if (parsedPage !== currentPage) {
+      setDesiredPage(parsedPage);
+    }
     if (totalPages > 1) {
-      if (parsedPage < 1) {
+      if (desiredPage < 1) {
         setCurrentPage(1);
-        navigate(`?page=1${searchParam ? `&search=${searchParam}` : ''}`);
-      } else if (parsedPage > totalPages) {
+        navigate(`${location.pathname}?page=1${searchValue ? `&search=${searchValue}` : ''}`);
+      } else if (desiredPage > totalPages) {
         setCurrentPage(totalPages);
       } else {
-        setCurrentPage(parsedPage);
+        setCurrentPage(desiredPage);
+        navigate(`${location.pathname}?page=${desiredPage}${searchValue ? `&search=${searchValue}` : ''}`);
       }
     } else {
       setCurrentPage(1);
+      navigate(`${location.pathname}?page=1${searchValue ? `&search=${searchValue}` : ''}`);
     }
-  }, [totalPages, parsedPage, navigate, setCurrentPage, searchParam]);
+  }, [location.pathname, pageParam, navigate, filteredDataLength, pageSize, searchValue]);
 
-  useEffect(() => {
-    navigate(`?page=${currentPage}${searchParam ? `&search=${searchParam}` : ''}`);
-  }, [currentPage, searchParam, navigate]);
-
-  useEffect(() => {
-    if (filterValue !== searchParam) {
-      setCurrentPage(1);
-    }
-  }, [filterValue, searchParam, setCurrentPage]);
+  return {
+    currentPage,
+    setCurrentPage,
+    setDesiredPage,
+  };
 };
