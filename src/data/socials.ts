cd5d8e50940a1a73b3fac2d05f3e29cbc16eb07c
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-07-31 23:49:24 +0000

    modified url for socials thumbnails

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,22 +1,22 @@
 export const socials: { name: string; imageSource: string; link: string }[] = [
   {
     name: 'LinkedIn',
-    imageSource: '../../assets/images/linkedin-profile.png',
+    imageSource: '/assets/images/linkedin-profile.png',
     link: 'https://www.linkedin.com/in/joseph-w-valdez/',
   },
   {
     name: 'Portfolio',
-    imageSource: '../../assets/images/portfolio.png',
+    imageSource: '/assets/images/portfolio.png',
     link: 'http://jwv-portfolio.herokuapp.com/',
   },
   {
     name: 'GitHub',
-    imageSource: '../../assets/images/github-profile.png',
+    imageSource: '/assets/images/github-profile.png',
     link: 'https://github.com/joseph-w-valdez',
   },
   {
     name: 'Repository',
-    imageSource: '../../assets/images/repository.png',
+    imageSource: '/assets/images/repository.png',
     link: 'https://github.com/joseph-w-valdez/Valorant-Checker',
   },
 ];
